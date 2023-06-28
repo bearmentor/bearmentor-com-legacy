cfb120cb8657e929c9018c261dc4b0dc684f91b0
M Haidar Hanif <git@mhaidarhanif.com>
2023-06-29 02:08:46 +0700

    chore: eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    "@remix-run/eslint-config",
+    "@remix-run/eslint-config/node",
+    "prettier",
+  ],
+  plugins: ["prettier"],
+  rules: {
+    "react/self-closing-comp": ["warn", { component: true, html: true }],
+  },
 };
