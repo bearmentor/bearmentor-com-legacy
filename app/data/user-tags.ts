d2b7c84e550fa33000ff9d750f8b3b78fb4fca34
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-17 16:06:21 +0700

    feat: refine users and user tags seed

--- app/data/user-tags.ts
+++ app/data/user-tags.ts
@@ -1,9 +1,23 @@
 import type { UserTag } from "@prisma/client"
 
-export type DataUserTag = Pick<UserTag, "symbol" | "name"> & {
+export type DataUserTag = Pick<UserTag, "name"> & {
+  symbol: DataUserTagSymbol
   sequence?: UserTag["sequence"]
   description?: UserTag["description"]
 }
+
+export type DataUserTagSymbol =
+  | "ARTIST"
+  | "COLLABORATOR"
+  | "COMPANY"
+  | "DESIGNER"
+  | "DEVELOPER"
+  | "FOUNDER"
+  | "MARKETER"
+  | "MENTEE"
+  | "MENTOR"
+  | "ORGANIZATION"
+  | "RECRUITER"
 
 export const dataUserTags: DataUserTag[] = [
   {
@@ -24,10 +38,10 @@ export const dataUserTags: DataUserTag[] = [
     sequence: 3,
     description: "Someone who want to be mentored by the mentors",
   },
-  { symbol: "RECRUITER", name: "Recruiter" },
   { symbol: "DEVELOPER", name: "Developer" },
   { symbol: "DESIGNER", name: "Designer" },
   { symbol: "MARKETER", name: "Marketer" },
+  { symbol: "RECRUITER", name: "Recruiter" },
   { symbol: "ARTIST", name: "Artist" },
   { symbol: "FOUNDER", name: "Founder" },
   { symbol: "ORGANIZATION", name: "Organization" },
--- app/data/users.ts
+++ app/data/users.ts
@@ -1,14 +1,19 @@
 import type { User, UserProfile } from "@prisma/client"
 
+import type { DataUserTagSymbol } from "~/data"
+
 export type DataUser = Pick<User, "name" | "username" | "nick"> & {
+  tags?: DataUserTagSymbol[]
+} & {
   profiles: { create: DataUserProfile | DataUserProfile[] }
 }
 
 export type DataUserProfile = Pick<
   UserProfile,
-  "headline" | "bio" | "modeName" | "sequence"
+  "headline" | "bio" | "modeName"
 > & {
   isPrimary?: UserProfile["isPrimary"]
+  sequence?: UserProfile["sequence"]
 }
 
 export const dataUsers: DataUser[] = [
@@ -16,13 +21,14 @@ export const dataUsers: DataUser[] = [
     name: "M Haidar Hanif",
     username: "haidar",
     nick: "Haidar",
+    tags: ["COLLABORATOR", "MENTOR", "DEVELOPER"],
     profiles: {
       create: [
         {
           headline: "Software Engineering Mentor",
           bio: "Helping you to learn and build something for good on the web.",
           modeName: "Mentor",
-          isPrimary: true,
+
           sequence: 1,
         },
         {
@@ -38,12 +44,13 @@ export const dataUsers: DataUser[] = [
     name: "Zain Fathoni",
     username: "zain",
     nick: "Zain",
+    tags: ["COLLABORATOR", "MENTOR", "DEVELOPER"],
     profiles: {
       create: {
         headline: "Software Engineer",
         bio: "Frontend at Ninja Van",
         modeName: "Engineer",
-        isPrimary: true,
+
         sequence: 1,
       },
     },
@@ -52,13 +59,12 @@ export const dataUsers: DataUser[] = [
     name: "Naufaldi Rafif",
     username: "faldi",
     nick: "Faldi",
+    tags: ["COLLABORATOR", "MENTOR", "DEVELOPER"],
     profiles: {
       create: {
         headline: "Frontend Web Developer",
         bio: "Ningen (人間) in Tech",
         modeName: "Developer",
-        isPrimary: true,
-        sequence: 1,
       },
     },
   },
@@ -66,13 +72,12 @@ export const dataUsers: DataUser[] = [
     name: "Kenneth Mahakim",
     username: "kenneth",
     nick: "Kenneth",
+    tags: ["COLLABORATOR", "MENTOR", "DESIGNER"],
     profiles: {
       create: {
         headline: "UI and UX Designer",
         bio: "Bridging interface and experience",
         modeName: "Designer",
-        isPrimary: true,
-        sequence: 1,
       },
     },
   },
@@ -80,13 +85,12 @@ export const dataUsers: DataUser[] = [
     name: "Ahmad Marzuki",
     username: "amadzuki",
     nick: "Marzuki",
+    tags: ["MENTEE", "DEVELOPER"],
     profiles: {
       create: {
         headline: "Frontend Engineer",
         bio: "Implementing UI",
         modeName: "Engineer",
-        isPrimary: true,
-        sequence: 1,
       },
     },
   },
@@ -94,13 +98,12 @@ export const dataUsers: DataUser[] = [
     name: "Krishna Rowter",
     username: "krowter",
     nick: "Krishna",
+    tags: ["MENTEE", "DEVELOPER"],
     profiles: {
       create: {
         headline: "Frontend Engineer at Vidio.com",
         bio: "Building Websites",
         modeName: "Engineer",
-        isPrimary: true,
-        sequence: 1,
       },
     },
   },
@@ -108,13 +111,12 @@ export const dataUsers: DataUser[] = [
     name: "Rizky Zhang",
     username: "rizkyzhang",
     nick: "Rizky",
+    tags: ["MENTEE", "DEVELOPER"],
     profiles: {
       create: {
         headline: "Sofware Engineer at Ubersnap",
         bio: "I have worked in complex applications such as e-commerce and event management service",
         modeName: "Engineer",
-        isPrimary: true,
-        sequence: 1,
       },
     },
   },
@@ -122,13 +124,12 @@ export const dataUsers: DataUser[] = [
     name: "Ali Reza Yahya",
     username: "alileza",
     nick: "Ali",
+    tags: ["MENTEE", "DEVELOPER"],
     profiles: {
       create: {
         headline: "Sofware Engineer at SadaPay",
         bio: "Working as a Senior Software Engineer (Infrastructure)",
         modeName: "Engineer",
-        isPrimary: true,
-        sequence: 1,
       },
     },
   },
