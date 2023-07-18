a212b20299b03aa084f88c99a24f1ff2c39064c8
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-18 07:39:06 +0700

    feat: prepare for user secret credentials

---
+++ app/data/README.md
@@ -0,0 +1,19 @@
+# Data for Credentials
+
+Create `users-credentials.json` with this format:
+
+```json
+[
+  {
+    "username": "username",
+    "email": "user1@example.com",
+    "password": "set_the_password_1"
+  },
+  {
+    "username": "username2",
+    "email": "user2@example.com",
+    "password": "set_the_password_2"
+  }
+  // ...
+]
+```
--- app/routes/api.usernames._index.tsx
+++ app/routes/api.usernames._index.tsx
@@ -4,15 +4,8 @@ import { prisma } from "~/libs"
 
 export const loader = async ({ request }: LoaderArgs) => {
   const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      name: true,
-      username: true,
-      role: { select: { symbol: true } },
-      profiles: { select: { headline: true, links: true } },
-    },
+    select: { username: true },
     orderBy: { createdAt: "asc" },
   })
-
   return json({ users })
 }
