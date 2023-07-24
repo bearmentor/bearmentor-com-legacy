9d508d50068e07ed07d0087e6302324705c95cb8
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-24 13:23:47 +0700

    feat: settings text work in progress

--- app/routes/settings.account.tsx
+++ app/routes/settings.account.tsx
@@ -3,6 +3,7 @@ export default function Route() {
     <div className="w-full space-y-10">
       <header>
         <h2>Account</h2>
+        <p className="text-muted-foreground">(Work in progress)</p>
       </header>
     </div>
   )
