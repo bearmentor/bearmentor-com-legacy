e3698b7e7ba0c0896763a523797d6b35c53ed1a8
M Haidar Hanif <git@mhaidarhanif.com>
2023-08-07 04:29:21 +0700

    refactor: move pluralize to string utils

--- app/utils/string.ts
+++ app/utils/string.ts
@@ -1,3 +1,9 @@
+import pluralize from "pluralize"
+
+export function formatPluralItems(word: string, count: number) {
+  return pluralize(word, count, true)
+}
+
 export function getNameInitials(name = "First Last") {
   return name
     .trim()
@@ -11,9 +17,7 @@ export function getNameInitials(name = "First Last") {
 
 export function truncateText(text: string, maxLength = 200) {
   if (!text || typeof text !== "string") return text
-  return text.length > maxLength
-    ? text.substring(0, maxLength - 3) + "..."
-    : text
+  return text.length > maxLength ? text.substring(0, maxLength - 3) + "..." : text
 }
 
 export function capitalizeText(text: string) {
