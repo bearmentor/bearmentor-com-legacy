c0f21ae7c6b860838f8ed70e42972314cd0aefc7
M Haidar Hanif <git@mhaidarhanif.com>
2023-08-16 00:39:17 +0700

    chore: upgrade dependencies

--- taze.config.js
+++ taze.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from "taze"
 export default defineConfig({
   write: true,
   install: true,
-  exclude: ["prettier", "nanoid"],
+  exclude: [
+    "nanoid",
+    "prettier",
+    "prettier-plugin-tailwindcss",
+    "@conform-to/react",
+    "@conform-to/zod",
+  ],
   packageMode: { "/remix/": "latest" },
 })
